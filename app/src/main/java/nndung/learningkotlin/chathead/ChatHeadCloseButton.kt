b88c8802ba2ec5df14175e1523f605f1061b1801
ADMIN\nndun <acquybongdem00@gmail.com>
2017-08-11 08:51:40 +0700

    Add close button

---
+++ app/src/main/java/nndung/learningkotlin/chathead/ChatHeadCloseButton.kt
@@ -0,0 +1,13 @@
+package nndung.learningkotlin.chathead
+
+import android.content.Context
+import android.widget.ImageView
+
+/**
+ * Created by nndun on 8/11/2017.
+ */
+class ChatHeadCloseButton : ImageView {
+    constructor(context: Context) : super(context) {
+
+    }
+}
